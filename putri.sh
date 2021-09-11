30c8c96eaefab84063febf1410593c995f9e749c
Mr.T0B1 <69340167+tobiphoenix@users.noreply.github.com>
2021-09-11 13:51:33 +0700

    Create putri.sh

---
+++ putri.sh
@@ -0,0 +1,97 @@
+#warna
+N='\033[0m'
+R='\033[0;31m'
+G='\033[0;32m'
+O='\033[0;33m'
+P="\033[2m"
+BL='\033[0;96m\033[2m'
+B='\033[1;36m'
+BR="\033[0;34m"
+U="\033[0;35m"
+clear
+echo -e "Nanti kalo ada yang tanya Y/n kamu ketik y abis itu enter ya cantik mwwah sayangg"
+sleep 4
+sudo apt-get install lolcat
+sudo apt-get install figlet
+sudo apt-get install toilet
+pkg install figlet
+pkg install lolcat
+pkg install toilet
+pkg install ruby
+gem install lolcat
+
+clear
+keyowner="putri"
+keymember="membertobi"
+read -p "nama mu sopo: " key;
+echo
+if [ $key == $keyowner ]
+then
+botsqli="kangen"
+clear
+echo -e "Hai cayang jadi kan semenjak kemarin itu kan mamaku ngomong gaboleh chatan dll jadi kita kaya makin jauh gitu terus kamu khawatirr teruss aku pgn buat kamu ga khawatir dehh , aku punya pertanyaan nichh" | lolcat
+sleep 5
+echo "1"
+sleep 1 
+echo "2"
+sleep 1 
+echo "3"
+sleep 2
+clear
+else
+clear
+echo -e "MBOH GAKENAL KON SOPO KETIK NAMA ASLI (PUTRI) NGONO "
+sleep 5
+exit
+bash test.sh
+fi
+echo -e "Tobi:Ayang kangen ga sama aku?"
+read -p "Putri:" botapa;
+if [ $botapa == $botsqli ]
+then 
+clear
+echo -e "aku juga kangennn banget puolll"
+sleep 3
+clear
+else
+clear
+echo -e "ketik kangen ngono lo "
+exit
+bash test.sh
+fi
+kangentitik="titik"
+kangenwakeh="wakeh"
+echo -e "Tobi:Kangen e titik apa wakeh?"
+read -p "Putri:" kangen;
+echo
+if [ $kangen == $kangentitik ]
+then
+clear
+echo -e "KOK TITIK SEH WAKEH NGONO"
+elif [ $kangen == $kangenwakeh ]
+then
+clear
+echo -e "aku yo kangennn puolllllll aa mwwahhhhhhh"
+echo -e "tunggu yaaaa"
+sleep 4
+clear
+figlet"
+_____$$$$_________$$$$
+___$$$$$$$$_____$$$$$$$$
+_$$$$$$$$$$$$_$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$
+_$$$$$$$$$$$$$$$$$$$$$$$$$
+__$$$$$$$$$$$$$$$$$$$$$$$
+____$$$$$$$$$$$$$$$$$$$
+_______$$$$$$$$$$$$$
+__________$$$$$$$
+____________$$$
+_____________$
+
+" | lolcat
+else
+echo -e "PILIH SG BENER KETIK TITIK/WAKEH"
+sleep 3
+exit
+fi
